d0d1a3484d91c0e97d21abf6792b862fe118eb82
Adegboyega michael <83494187+adegboyegaayomide@users.noreply.github.com>
2023-09-26 23:37:34 +0100

    Add files via upload

---
+++ password generator/password.py
@@ -0,0 +1,16 @@
+password = int(input(" how many digit of password did you want:"))
+if password == 10:
+     print("jki566yhh$")
+elif  password == 9 :
+     print("bdgh34po%")
+elif  password == 8 :
+     print("6577yh8#")
+elif password == 7 :
+    print("767ggh$")
+elif password == 6:
+    print ("6751er")
+elif password == 5:
+    print("657ty")
+elif password < 5 :
+    print("password can only be greater than 5 ")
+     
